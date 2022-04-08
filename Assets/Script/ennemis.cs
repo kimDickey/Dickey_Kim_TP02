c815dbd185061000160e614db046e588b2ba3778
Kim Dickey <1783847@cegepat.qc.ca>
2022-04-08 11:00:27 -0400

    ennemi

--- Assets/Script/ennemis.cs
+++ Assets/Script/ennemis.cs
@@ -5,15 +5,31 @@ using UnityEngine.AI;
 public class ennemis : MonoBehaviour
 {
     NavMeshAgent agent;
+    public GameObject prefabEnnemi1;
+    int hitpoints;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        Vector3 newDestination = new Vector3(16.4f, 0f, -52.8f);
+        agent.SetDestination(newDestination);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void Shot()
+    {
+        hitpoints--;
+
+        if (hitpoints < 1)
+            Die();
+    }
+    public void Die()
+    {
+        CancelInvoke("UpdateDestination");
+        Destroy(gameObject);
     }
 }
